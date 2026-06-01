f50456ea8475fcdf38ecdf29474bf1a41aaeb773
nairx <nairxprv@gmail.com>
2026-06-01 11:19:54 +0530

    updated

--- part1/index1.js
+++ part1/index1.js
@@ -1,3 +1,11 @@
-function f1(){
-    alert("Hello World")
+function f1() {
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    if (email === "john@gmail.com" && password === "1234") {
+        alert("Welcome")
+    }
+    else {
+        alert("Access Denied")
+    }
+
 }
