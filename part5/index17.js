28a6c0aed5e6acd2ab9144330f89280bbece5053
nairx <nairxprv@gmail.com>
2026-06-02 14:49:36 +0530

    updated

--- part5/index17.js
+++ part5/index17.js
@@ -1,7 +1,16 @@
 const products = [
-  { id:1, name: "Laptop", price: 75000 },
-  { id:2, name: "Desktop", price: 25000 },
-  { id:3, name: "Keyboard", price: 1000 },
+    { id: 1, name: "Laptop", price: 75000 },
+    { id: 2, name: "Desktop", price: 25000 },
+    { id: 3, name: "Keyboard", price: 1000 },
 ];
-const newArr = products.map((product)=> ({...product}))
+const newArr = products.map((product) => product.id===2 ? ({ ...product, price: product.price + 500 }) : product)
 console.log(newArr)
+
+
+// const products = [
+//     { id: 1, name: "Laptop", price: 75000 },
+//     { id: 2, name: "Desktop", price: 25000 },
+//     { id: 3, name: "Keyboard", price: 1000 },
+// ];
+// const newArr = products.map((product) => ({ ...product, price: product.price + 500 }))
+// console.log(newArr)
